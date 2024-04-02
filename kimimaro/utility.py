ffdc090bc4c0bf49f1f69e5a0e0a4912a22f654c
William Silversmith <william.silversmith@gmail.com>
2024-04-01 21:58:09 -0400

    feat: propogate "mode" argument to np.pad

--- kimimaro/utility.py
+++ kimimaro/utility.py
@@ -189,7 +189,7 @@ def cross_sectional_area(
   return skeletons
 
 # From SO: https://stackoverflow.com/questions/14313510/how-to-calculate-rolling-moving-average-using-python-numpy-scipy
-def moving_average(a:np.ndarray, n:int) -> np.ndarray:
+def moving_average(a:np.ndarray, n:int, mode:str = "symmetric") -> np.ndarray:
   if n <= 0:
     raise ValueError(f"Window size ({n}), must be >= 1.")
   elif n == 1:
@@ -199,9 +199,9 @@ def moving_average(a:np.ndarray, n:int) -> np.ndarray:
     return a
 
   if a.ndim == 2:
-    a = np.pad(a, [[n, n],[0,0]], mode="symmetric")
+    a = np.pad(a, [[n, n],[0,0]], mode=mode)
   else:
-    a = np.pad(a, [n, n], mode="symmetric")
+    a = np.pad(a, [n, n], mode=mode)
 
   ret = np.cumsum(a, dtype=float, axis=0)
   ret = (ret[n:] - ret[:-n])[:-n]
