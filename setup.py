ca63cce739484f07cfe810439024ab790c682612
William Silversmith <william.silversmith@gmail.com>
2025-04-29 19:29:23 -0400

    fix: windows compilation, remove --fast-math

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import setuptools
+import sys
 
 class NumpyImport:
   def __repr__(self):
@@ -10,6 +11,19 @@ class NumpyImport:
 
   __fspath__ = __repr__
 
+extra_compile_args = []
+if sys.platform == 'win32':
+  extra_compile_args += [
+    '/std:c++17', '/O2'
+  ]
+else:
+  extra_compile_args += [
+    '-std=c++17', '-O3'
+  ]
+
+if sys.platform == 'darwin':
+  extra_compile_args += [ '-stdlib=libc++', '-mmacosx-version-min=10.9' ]
+
 setuptools.setup(
     ext_modules=[
       setuptools.Extension(
@@ -17,9 +31,7 @@ setuptools.setup(
         sources=[ './ext/skeletontricks/skeletontricks.pyx' ],
         language='c++',
         include_dirs=[ str(NumpyImport()) ],
-        extra_compile_args=[
-          '-std=c++17', '-O3', '-ffast-math'
-        ]
+        extra_compile_args=extra_compile_args,
       ),
     ],
 )
