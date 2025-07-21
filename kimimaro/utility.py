568de5c62d9293e0b35151e5ba3962028da86321
William Silversmith <william.silversmith@gmail.com>
2025-07-21 14:51:38 -0400

    perf: use xs3d persistent data feature

--- kimimaro/utility.py
+++ kimimaro/utility.py
@@ -209,6 +209,9 @@ def cross_sectional_area(
   }
 
   def cross_sectional_area_helper(skel, binimg, roi):
+
+    xs3d.set_shape(binimg)
+
     cross_sections = None
     if visualize_section_planes:
       cross_sections = np.zeros(binimg.shape, dtype=np.uint32, order="F")
@@ -277,6 +280,7 @@ def cross_sectional_area(
             binimg, vert, 
             normal, anisotropy,
             return_contact=True,
+            use_persistent_data=True,
           )
           if repair_contacts:
             contacts[idx] = contact
@@ -301,11 +305,14 @@ def cross_sectional_area(
     skel.cross_sectional_area = areas
     skel.cross_sectional_area_contacts = contacts
 
-  shape_iterator(
-    all_labels, skeletons, 
-    fill_holes, in_place, progress, 
-    cross_sectional_area_helper
-  )
+  try:
+    shape_iterator(
+      all_labels, skeletons, 
+      fill_holes, in_place, progress, 
+      cross_sectional_area_helper
+    )
+  finally:
+    xs3d.clear_shape()
 
   if hasattr(skeletons, "vertices"):
     skelitr = [ skeletons ]
