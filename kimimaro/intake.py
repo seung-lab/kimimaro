0acd38cd9d8153a50001b3365a94ad34e2def521
William Silversmith <william.silversmith@gmail.com>
2025-10-03 17:24:08 -0400

    fix: don't even reference vg_mmap

--- kimimaro/intake.py
+++ kimimaro/intake.py
@@ -259,7 +259,7 @@ def skeletonize(
       cc_mmap.close()
       shm.unlink(dbf_shm_location)
       shm.unlink(cc_shm_location)
-      if vg_mmap is not None:
+      if voxel_graph is not None:
         vg_mmap.close()
         shm.unlink(vg_shm_location)
 
